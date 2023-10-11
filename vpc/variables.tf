1519d2787591fe08e3f2289f325eac4535287330
Amina Ibrahim <aminaibrahim@Abus-Air.attlocal.net>
2023-10-11 14:31:00 -0400

    cleaning up files

--- vpc/variables.tf
+++
@@ -1,8 +0,0 @@
-# environment variables
-variable "region" {}
-variable "project_name" {}
-variable "environment" {}
-
-# vpc variables 
-variable "vpc_cidr" {}
-variable "public_subnet_az1 _cidr" {}
